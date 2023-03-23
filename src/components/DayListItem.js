a09cd335d16400ef45b5fc52a3dfc056da989dd1
thahira97 <thahirasheerin3@gmail.com>
2023-03-23 11:20:20 -0400

    Render spots text as per avilablity

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,15 +3,25 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
- const dayClass = classNames("day-list__item", {
-  "day-list__item--selected": props.selected,
-  "day-list__item--full": props.spots === 0,
- })
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
+
+  const formatSpots = function (props) {
+    if (props.spots === 0) {
+      return <h3 className="text--light">no spots remaining</h3>;
+    } else if (props.spots === 1) {
+      return <h3 className="text--light">1 spot remaining</h3>;
+    } else {
+      return <h3 className="text--light">{props.spots} spots remaining</h3>;
+    }
+  };
+
   return (
-    <li onClick={()=>props.setDay(props.name)} className={dayClass}
-    >
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2>
+      {formatSpots(props)}
     </li>
   );
 }
