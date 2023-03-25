4a4aef21997d3e9df00f065c8bbaed3d7845bff9
thahira97 <thahirasheerin3@gmail.com>
2023-03-25 14:37:13 -0400

    Refactor formatSpots function to accept just the spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,10 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0,
   });
 
-  const formatSpots = function (props) {
-    if (props.spots === 0) {
+  const formatSpots = function (spots) {
+    if (spots === 0) {
       return <h3 className="text--light">no spots remaining</h3>;
-    } else if (props.spots === 1) {
+    } else if (spots === 1) {
       return <h3 className="text--light">1 spot remaining</h3>;
     } else {
       return <h3 className="text--light">{props.spots} spots remaining</h3>;
@@ -19,9 +19,9 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li onClick={() => props.setDay(props.name)} className={dayClass} >
+    <li onClick={() => props.setDay(props.name)} className={dayClass} selected={props.selected} >
       <h2 className="text--regular">{props.name}</h2>
-      {formatSpots(props)}
+      {formatSpots(props.spots)}
     </li>
   );
 }
