7086a84642b4f422f8c3b4d5611d8a022e0108a6
thahira97 <thahirasheerin3@gmail.com>
2023-03-26 22:00:50 -0400

    Add header and empty component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "components/Appointment/style.scss"
+export default function Appointment (props){
+  return(
+    <article className="appointment"></article>
+  )
+}
