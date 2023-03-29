d3ac5b4784d3383fc312a9c7bce0ba8b439b6ebf
thahira97 <thahirasheerin3@gmail.com>
2023-03-29 09:49:12 -0400

    Siaply the apoointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import Appointment from "components/Appointment"
 import "components/Application.scss";
 import DayList from "./DayList";
 
@@ -20,9 +21,56 @@ const days = [
     spots: 0,
   },
 ];
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const dailyAppointments = Object.values(appointments).map((appointment)=> {
+    return(
+      <Appointment
+      key={appointment.id} 
+      {...appointment} 
+    />
+    )
+  }) 
+  dailyAppointments.push(<Appointment key="last" time="5pm" />)
   return (
     <main className="layout">
       <section className="sidebar">
@@ -46,7 +94,7 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        
+        {dailyAppointments}
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "components/Appointment/style.scss"
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 export default function Appointment (props){
   return(
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>}
+    </article>
   )
 }
