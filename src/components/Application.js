2bacd25b3146b5e7f8ff808183070e952a2fdd9b
thahira97 <thahirasheerin3@gmail.com>
2023-03-31 20:02:23 -0400

    feat:Fetch days from api/days using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,10 @@
 import React from "react";
-import { useState } from "react";
+import axios from 'axios';
+import { useState, useEffect } from "react";
 import Appointment from "components/Appointment"
 import "components/Application.scss";
 import DayList from "./DayList";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 const appointments = {
   "1": {
     id: 1,
@@ -62,6 +46,14 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+  useEffect(()=> {
+    axios
+      .get("/api/days")
+      .then((response)=> {
+        setDays(response.data)
+      })
+    }, [])
   const dailyAppointments = Object.values(appointments).map((appointment)=> {
     return(
       <Appointment
