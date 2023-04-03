83a91eaec76946e17863a61ee01b4f245665dd6a
thahira97 <thahirasheerin3@gmail.com>
2023-04-03 11:27:53 -0400

    feat:Add useVisualMode.js file and the tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -13,6 +13,7 @@ export function getAppointmentsForDay(state, day) {
 
   return appointments;
 }
+
 export function getInterview(state, interview) {
 if (!interview){
   return null
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (replace) {
+      setMode(newMode);
+      setHistory((prev) => prev.slice(1), [...newMode]);
+    }
+    setMode(newMode);
+    setHistory((prev) => [newMode, ...prev]);
+  }
+  function back() {
+    if (history.length > 1) {
+      setMode(history[1]);
+      setHistory((prev) => prev.slice(1));
+    }
+  }
+
+  return { mode, transition, back };
+}
