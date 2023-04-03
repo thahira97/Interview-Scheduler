2ba5f4980acf0f68414ee97913ac4ba1bad18fa7
thahira97 <thahirasheerin3@gmail.com>
2023-04-03 12:26:03 -0400

    feat:Use transaction to particulary render the components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,36 @@
 import React from "react";
-import "components/Appointment/style.scss"
+import "components/Appointment/style.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
-export default function Appointment (props){
-  return(
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
+
+export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  return (
     <article className="appointment">
-      <Header time={props.time}/>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>}
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => {
+            back(EMPTY);
+          }}
+        />
+      )}
     </article>
-  )
+  );
 }
