c37354656f67446f56039e48ab1434b937436bd9
thahira97 <thahirasheerin3@gmail.com>
2023-04-13 11:27:28 -0400

    feat:Add proptypes

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -14,8 +14,6 @@ export default function Form(props) {
     reset();
     props.onCancel();
   };
-  console.log({interviewer})
-
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,8 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
-export default function InterviewerList(props) {
-
+function InterviewerList(props) {
   const interviewerArray = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
@@ -10,7 +10,7 @@ export default function InterviewerList(props) {
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.value}
-        setInterviewer={() => props.onChange(interviewer.id)}    
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
   });
@@ -21,3 +21,8 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+ InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired,
+  };
+
+  export default InterviewerList;
