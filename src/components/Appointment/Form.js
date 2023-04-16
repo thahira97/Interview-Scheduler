cd26caf2df13ca2c5877e2a1fa25cf7a005413ff
thahira97 <thahirasheerin3@gmail.com>
2023-04-16 13:35:31 -0400

    eat:Add tests for form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,27 +6,45 @@ import Button from "components/Button";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
   const reset = () => {
     setStudent("");
+    setError("");
     setInterviewer(null);
   };
   const cancel = () => {
     reset();
     props.onCancel();
   };
+  function validate() {
+     if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    } 
+
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    setError("");
+    props.onSave(student, interviewer);
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form onSubmit={(event) => event.preventDefault()} autoComplete="off">
           <input
+            data-testid="student-name-input"
             className="appointment__create-input text--semi-bold"
-            name="name"
+            name="student"
             type="text"
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
@@ -39,10 +57,8 @@ export default function Form(props) {
             Cancel
           </Button>
           <Button
-            onClick={() => {
-              props.onSave(student, interviewer);
-            }}
             confirm
+            onClick={validate}
           >
             Save
           </Button>
