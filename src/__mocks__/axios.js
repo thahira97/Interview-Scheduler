20f8e5b05ba184c6eacf7e5c8ddcc997c87ede70
thahira97 <thahirasheerin3@gmail.com>
2023-04-17 23:54:54 -0400

    feat:Add tests for canceling the interview

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -86,4 +86,11 @@ export default {
       statusText: "No Content",
     });
   }),
+
+  delete: jest.fn((url) => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content",
+    });
+  }),
 };
