2440378196c2f743201d323939aac9c6a36374c1
thahira97 <thahirasheerin3@gmail.com>
2023-04-19 23:50:36 -0400

    Refactor code

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -80,14 +80,14 @@ export default {
       });
     }
   }),
-  put: jest.fn(() => {
+  put: jest.fn((url) => {
     return Promise.resolve({
       status: 204,
       statusText: "No Content",
     });
   }),
 
-  delete: jest.fn(() => {
+  delete: jest.fn((url) => {
     return Promise.resolve({
       status: 204,
       statusText: "No Content",
