5ee7e13f6edd140e998659a092cf7ac7e7baa12c
thahira97 <thahirasheerin3@gmail.com>
2023-04-19 22:09:38 -0400

    fix:fix indentations for helpers

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,5 @@
 export function getAppointmentsForDay(state, day) {
-  const appointmentArr = state.days.filter(dayItem => dayItem.name === day);
+  const appointmentArr = state.days.filter((dayItem) => dayItem.name === day);
 
   if (appointmentArr.length === 0) {
     return [];
@@ -8,26 +8,25 @@ export function getAppointmentsForDay(state, day) {
   const appointmentIds = appointmentArr[0].appointments;
 
   const appointments = appointmentIds.map(
-    appointmentId => state.appointments[appointmentId]
+    (appointmentId) => state.appointments[appointmentId]
   );
 
   return appointments;
 }
 
 export function getInterview(state, interview) {
-if (!interview){
-  return null
-}
-else {
-  return {
-    student: interview.student,
-    interviewer : state.interviewers[interview.interviewer]
+  if (!interview) {
+    return null;
+  } else {
+    return {
+      student: interview.student,
+      interviewer: state.interviewers[interview.interviewer],
+    };
   }
 }
-}
 
-export function getInterviewersForDay (state, day){
-  const interviewerArr = state.days.filter(dayItem => dayItem.name === day);
+export function getInterviewersForDay(state, day) {
+  const interviewerArr = state.days.filter((dayItem) => dayItem.name === day);
 
   if (interviewerArr.length === 0) {
     return [];
@@ -36,7 +35,7 @@ export function getInterviewersForDay (state, day){
   const interviewerIds = interviewerArr[0].interviewers;
 
   const interviewers = interviewerIds.map(
-    interviewerId => state.interviewers[interviewerId]
+    (interviewerId) => state.interviewers[interviewerId]
   );
 
   return interviewers;
